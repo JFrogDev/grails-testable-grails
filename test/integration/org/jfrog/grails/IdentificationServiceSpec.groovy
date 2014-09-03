58b7b018bb19790e9ce3447ef34b392a50fb182c
noamt <noamt@jfrog.com>
2014-09-03 16:09:17 +0300

    Add a data-driven case to the integration test mixin

--- test/integration/org/jfrog/grails/IdentificationServiceSpec.groovy
+++ test/integration/org/jfrog/grails/IdentificationServiceSpec.groovy
@@ -2,6 +2,7 @@ package org.jfrog.grails
 
 import grails.test.spock.IntegrationSpec
 import spock.lang.Shared
+import spock.lang.Unroll
 
 /**
  * @author Noam Y. Tenne
@@ -11,7 +12,7 @@ class IdentificationServiceSpec extends IntegrationSpec {
     @Shared
     IdentificationService identificationService
 
-    def 'Id'() {
+    def 'ID domains with one level of nesting'() {
         setup:
         def mal = new Person(name: 'Malcolm Reynolds').save(flush: true, failOnError: true)
         def serenity = new Spaceship(name: 'Serenity', type: 'Firefly', captain: mal).save(flush: true, failOnError: true)
@@ -22,5 +23,14 @@ class IdentificationServiceSpec extends IntegrationSpec {
         then:
         identifiedDomains.Spaceship == serenity.id
         identifiedDomains.subDomains.Person == mal.id
+    }
+
+    @Unroll
+    def 'ID an object that isn\'t a domain'() {
+        expect:
+        identificationService.identifyDomains(object) == [:]
+
+        where:
+        object << [identificationService, new Object()]
     }
 }
